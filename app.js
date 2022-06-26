c457a47616af8424e88dbbff4dd9064973ae0f87
Battlehart <sarah.stelljes@gmail.com>
2022-06-26 16:03:44 -0400

    fixed file path issue 2.

--- app.js
+++ app.js
@@ -307,6 +307,7 @@ const chooseFileDirectory = projectData => {
             message: `
             Enter a save path. 
             *Note: typically you would start off with "C:", but this may not always be the case.
+            *Hint: if you want to save to this folder dir, then don't put any text in
             `
         },
         {
@@ -322,6 +323,13 @@ const chooseFileDirectory = projectData => {
                 const search = '\\';
                 const replaceWith = '/';
                 projectData.fileDir = dirData.dir.split(search).join(replaceWith);
+                if(projectData.template === 'custom'){
+                    createCustomFiles(projectData);
+                } else {
+                    continueEditingProjectData(projectData);
+                }
+            } else if (dirData.dir === ''){
+                projectData.fileDir = './'
                 if(projectData.template === 'custom'){
                     createCustomFiles(projectData);
                 } else {
@@ -353,7 +361,7 @@ const createCustomFiles = projectData => {
             if(err) throw err;
         });
         // enter root folder
-        process.chdir(projectData.website);
+        process.chdir(projectData.website+'/'+projectData.website);
     }
     if(projectData.website.includes('_') && !projectData.website.includes('-')){
         const search = '_';
